4a3a245d5a532a6d34e25f06e726351c448dc249
HadesHappy <happyhades123@gmail.com>
2022-12-27 14:46:08 +0200

    aws npm

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const dev = {
 	baseURL: "https://main.d2wmt8jd1wat87.amplifyapp.com/api/",
-	landingPageUrl: "https://main.d2wmt8jd1wat87.amplifyapp.com/",
+	landingPageUrl: "https://main.d2wmt8jd1wat87.amplifyapp.com",
 	stripe: {
 		free: "price_1JcQD6HIZYvvnKladKayEvOZ",
 		entry: "price_1JL68HHIZYvvnKlaBJWS5uDe",
